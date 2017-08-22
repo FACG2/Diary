352ddf07a0034039a3a4c8d93217c7e408ff192d
Abdallah <you@example.com>
2017-08-22 15:47:09 +0300

    fix cb err

--- src/queries/validation.js
+++ src/queries/validation.js
@@ -6,7 +6,7 @@ const hashPassword = (password, cb) => {
     if (err) {
       cb(err);
     } else {
-      bcrypt.hash(password, salt, (err, hash) => {
+      bcrypt.hash(password, salt, (hash, err) => {
         if (err) {
           cb(err);
         } else {
@@ -18,7 +18,7 @@ const hashPassword = (password, cb) => {
 };
 
 const comparePasswords = (password, hashedPassword, cb) => {
-  bcrypt.compare(password, hashedPassword, (err, res) => {
+  bcrypt.compare(password, hashedPassword, (res, err) => {
     if (err) {
       cb(err);
     } else {
@@ -34,16 +34,16 @@ const validatePassword = (username, password, cb) => {
   };
   dbConnection.query(sql, (err, res) => {
     if (err) {
-      cb({isError: true, message: 'username already exists'});
+      cb(err);
     } else {
-      comparePasswords(password, res.rows[0], (err, res) => {
+      comparePasswords(password, res.rows[0], (res, err) => {
         if (err) {
           cb(err);
         } else {
           if (!res) {
-            cb({isError: true, message: 'password isn\'t correct'});
+            cb(err);
           } else {
-            cb({isError: true});
+            cb(err);
           }
         }
       });
@@ -56,14 +56,14 @@ const validateUserName = (username, cb) => {
     text: 'SELECT * FROM users where username= $1',
     values: [username]
   };
-  dbConnection.query(sql, (err, res) => {
+  dbConnection.query(sql, (res, err) => {
     if (err) {
-      cb({isError: true, message: err});
+      cb(err);
     } else {
       if (res.rows.length > 0) {
-        cb({isError: true, message: 'username already exists'});
+        cb(err);
       } else {
-        cb({isError: false});
+        cb(res.result);
       }
     }
   });
