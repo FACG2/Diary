d06a3134941299519770650d0e052b59a50c3e6d
moath <moaalnajjar@gmail.com>
2017-08-22 16:59:40 +0300

    create a test for router

--- test/router.test.js
+++ test/router.test.js
@@ -2,26 +2,23 @@ const test = require('tape');
 const shot = require('shot');
 const router = require('../src/router.js');
 
-
 test('Home rout returns a status code of 200', (t) => {
-	shot.inject(router,{method:'get',url:'/home'},(res)=>{
-		t.equal(res.statusCode,200,'respond with 200');
-		t.end();
-	})
-})
+  shot.inject(router, {method: 'get', url: '/home'}, (res) => {
+    t.equal(res.statusCode, 200, 'respond with 200');
+    t.end();
+  });
+});
 
+test('Home route returns a status code of 404', (t) => {
+  shot.inject(router, {method: 'get', url: '/afsdfh'}, (res) => {
+    t.equal(res.statusCode, 404, 'respond with 404');
+    t.end();
+  });
+});
 
-test('Home route returns a status code of 404',(t)=>{
-	shot.inject(router,{method:'get',url:'/afsdfh'},(res)=>{
-		t.equal(res.statusCode,404,'respond with 404');
-		 // t.deepEqual(res.payload,'<center><h1>page not found</h1></center>','should return error in the payload');
-		t.end();
-	})
-})
-
-test('Sign up route returns a status code of 200',(t)=>{
-	shot.inject(router,{method:'post',url:'/signup'},(res)=>{
-		t.equal(res.statusCode,200,'respond with 200');
-		t.end();
-	})
-})
+test('Sign up route returns a status code of 200', (t) => {
+  shot.inject(router, {method: 'post', url: '/signup'}, (res) => {
+    t.equal(res.statusCode, 200, 'respond with 200');
+    t.end();
+  });
+});
