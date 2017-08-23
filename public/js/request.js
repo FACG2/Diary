4dba3ad7217d42a0c94707aaa970ee90ceab2a16
Sohad Dader <sohadwd9@gmail.com>
2017-08-23 17:13:37 +0300

    add validation pass to frontend

---
+++ public/js/request.js
@@ -0,0 +1,24 @@
+var request = function (method, url, data, callback) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      var response = JSON.parse(xhr.responseText);
+      callback(xhr.status, response);
+    }
+  };
+  xhr.open(method, url, true);
+  if (method === 'GET') {
+    xhr.send();
+  } else {
+    xhr.send(data);
+  }
+};
+
+var get = function (url, cb) {
+  request('GET', url, null, cb);
+};
+
+var post = function (url, data, cb) {
+  request('POST', url, data, cb);
+};
